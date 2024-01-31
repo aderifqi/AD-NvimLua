a09325a2a5fd3f2395c85ff8cd03a8ed29183b8e
aderifqi <aderifqi85@gmail.com>
2024-01-31 21:34:51 +0700

    update

--- lua/lsp-config/language-servers.lua
+++ lua/lsp-config/language-servers.lua
@@ -33,8 +33,8 @@ local on_attach = function(client, bufnr)
 end
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol
-                                                                     .make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol
+                                                                      .make_client_capabilities())
 
 local lsp_flags = {
   -- This is the default in Nvim 0.7+
